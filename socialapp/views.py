55ee467871c2098c77180409d742d6060f41a28e
felkiriinya <felkiriinya@gmail.com>
2020-11-21 19:37:55 +0300

    added sign up and login forms and routes

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,7 +1,7 @@
-
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 # Create your views here.
-
+@login_required(login_url='/accounts/login/')
 def landing(request):
     pass
