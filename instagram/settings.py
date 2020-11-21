8bb105bd259a12704a8daf549254e5653cf3ddec
felkiriinya <felkiriinya@gmail.com>
2020-11-21 21:43:49 +0300

    added cloudinary configurations

--- instagram/settings.py
+++ instagram/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,6 +75,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'instagram.wsgi.application'
 
+cloudinary.config( 
+  cloud_name = "duhceor4r", 
+  api_key = "988552584751394", 
+  api_secret = "grnCc_TFy5WFWteERzMJRj3t88k" 
+)
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
--- socialapp/models.py
+++ socialapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from cloudinary.models import CloudinaryField
 
+import cloudinary
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_photo = CloudinaryField(upload_to='pics/',default='default.jpg')
+    bio = HTMLField(blank=True,default='I am a new user!')
+    
+class Image(models.Model):
+    image = CloudinaryField(upload_to='images/')
+    image_name = models.CharField(max_length=40)
+    image_caption = HTMLField() 
+    date_posted = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0,blank=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
