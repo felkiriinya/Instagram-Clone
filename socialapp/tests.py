49b1c7bbe30678e8550c23dfad5375a8b150c9f2
felkiriinya <felkiriinya@gmail.com>
2020-11-22 11:55:22 +0300

    created class image tests

--- socialapp/tests.py
+++ socialapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Image,Profile
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ImageTest(TestCase):
+    def setUp(self):
+        self.test_user= User(username = "Felista", password = "ilovemyself")
+        self.test_user.save()
+
+        self.test_image = Image(image='images/test.jpg', posted_by=self.test_user, date_posted=datetime.now())
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_image,Image))
+
+    def test_save(self):
+        self.test_image.save()
+        self.assertEqual(len(Image.objects.all()), 1)
+
+    def tearDown(self):
+        self.test_user.delete() 
+        Image.objects.all().delete()   
